7416db76bb629520f5863815c641cda95f341321
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2020-11-07 16:33:53 +0100

    samediv1

--- letouring/javascript/script.js
+++ letouring/javascript/script.js
@@ -11,3 +11,17 @@ function  cache()
       document.getElementById('intro').style.display= 'block'
     }
   }
+
+  function  affiche()
+  {
+    if (document.getElementById('montrer').innerHTML == 'montrer')
+    {
+      document.getElementById('montrer').innerHTML = 'cacher';
+      document.getElementById('article1').style.display= 'block';
+    }
+    else
+    {
+      document.getElementById('montrer').innerHTML = 'montrer'
+      document.getElementById('article1').style.display= 'none'
+    }
+  }
